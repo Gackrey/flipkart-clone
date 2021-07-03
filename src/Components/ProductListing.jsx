e981d18ce1f86034a653025d09882b79515c1b47
Gaurav <gauravdey625@gmail.com>
2021-07-03 17:39:43 +0530

    Added no products found message

--- src/Components/ProductListing.jsx
+++ src/Components/ProductListing.jsx
@@ -1,6 +1,6 @@
 import { Product } from "./Product";
 import { useCart } from "../Context/CartContext";
-import { getSortedData, filterData } from "../utils/DataFilter";
+import { getSortedData, filterData } from "./DataFilter";
 export function ProductListing() {
   const { Products, sortBy, filterSize, filterBrand, filterSex } = useCart();
   const getSorted = getSortedData(sortBy, Products);
@@ -8,18 +8,22 @@ export function ProductListing() {
   return (
     <div>
       <div className="all-prod">
-        {getFiltered.map((prod) => (
-          <Product
-            name={prod.name}
-            Image={prod.Image}
-            original_price={prod.original_price}
-            discount={prod.discount}
-            Overall_Price={prod.Overall_Price}
-            sizes={prod.sizes}
-            brand={prod.brand}
-            Ideal_for={prod.Ideal_for}
-          />
-        ))}
+        {getFiltered.length > 0 ? (
+          getFiltered.map((prod) => (
+            <Product
+              name={prod.name}
+              Image={prod.Image}
+              original_price={prod.original_price}
+              discount={prod.discount}
+              Overall_Price={prod.Overall_Price}
+              sizes={prod.sizes}
+              brand={prod.brand}
+              Ideal_for={prod.Ideal_for}
+            />
+          ))
+        ) : (
+          <h1>No products found</h1>
+        )}
       </div>
     </div>
   );
